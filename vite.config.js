25dcce663c4542b64b7ad2f2858e2fc292679fae
Jiaxin <luo.jx@outlook.com>
2026-02-24 04:38:48 +0800

    无

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig(async () => ({
   plugins: [sveltekit()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
